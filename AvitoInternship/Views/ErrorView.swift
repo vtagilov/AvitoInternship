d91417b2ea7cd4dace6343fbcfabbe9b39814e60
vtagilov <v.tagilov@mail.ru>
2024-09-12 11:16:16 +0300

    add ErrorView

---
+++ AvitoInternship/Views/ErrorView.swift
@@ -0,0 +1,64 @@
+//
+//  ErrorView.swift
+//  AvitoInternship
+//
+//  Created by Владимир on 12.09.2024.
+//
+
+import UIKit
+
+final class ErrorView: UIView {
+    
+    var tryAgainAction: (() -> Void)?
+    
+    private lazy var tryAgainButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.setTitle("Попробовать снова", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.addTarget(self, action: #selector(tryAgainButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "Нет интернета"
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        configureView()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func tryAgainButtonTapped() {
+        isHidden = true
+        tryAgainAction?()
+    }
+    
+    private func configureView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        isHidden = true
+        
+        for subview in [tryAgainButton, label] {
+            addSubview(subview)
+        }
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor),
+            label.topAnchor.constraint(equalTo: topAnchor),
+            
+            tryAgainButton.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 10),
+            tryAgainButton.bottomAnchor.constraint(lessThanOrEqualTo: bottomAnchor),
+            tryAgainButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+        ])
+    }
+}
--- AvitoInternship/Views/SearchViewController.swift
+++ AvitoInternship/Views/SearchViewController.swift
@@ -8,9 +8,65 @@
 import UIKit
 
 final class SearchViewController: UIViewController {
+    private let viewModel = SearchViewModel()
+    private let searchField = SearchField()
+    private let collectionView = SearchCollection()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .gray
+        configureView()
+        configureViewModel()
+        configureSearchView()
+    }
+    
+    private func configureViewModel() {
+        viewModel.loadingAction = { isLoading in
+            DispatchQueue.main.async {
+                self.searchField.setLoadingStatus(isLoading)
+            }
+        }
+        viewModel.searchErrorAction = { error in
+            DispatchQueue.main.async {
+                print("ERROR: ", error)
+            }
+        }
+        viewModel.searchResultAction = { model in
+            DispatchQueue.main.async {
+                self.collectionView.models.append(model)
+                self.collectionView.reloadData()
+            }
+        }
+    }
+    
+    private func configureSearchView() {
+        searchField.searchAction = { query in
+            self.viewModel.performSearchRequest(query: query)
+            self.collectionView.models = []
+        }
+        searchField.cellTypeAction = {
+            self.collectionView.type = self.collectionView.type == .list ? .table : .list
+            self.collectionView.reloadData()
+        }
+        searchField.sortButtonAction = { type in
+            self.viewModel.sortType = type
+        }
+    }
+    
+    private func configureView() {
+        for subview in [searchField, collectionView] {
+            view.addSubview(subview)
+        }
+        NSLayoutConstraint.activate([
+            searchField.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchField.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            searchField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchField.heightAnchor.constraint(equalToConstant: 35),
+            
+            collectionView.topAnchor.constraint(equalTo: searchField.bottomAnchor, constant: 5),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
 }
 
