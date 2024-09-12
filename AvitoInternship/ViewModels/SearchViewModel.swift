bfd26e78dd811afd9871b3c33e852eefe19afc9e
vtagilov <v.tagilov@mail.ru>
2024-09-12 10:31:40 +0300

    add SearchViewModel

---
+++ AvitoInternship/ViewModels/SearchViewModel.swift
@@ -0,0 +1,59 @@
+//
+//  SearchViewModel.swift
+//  AvitoInternship
+//
+//  Created by Владимир on 11.09.2024.
+//
+
+import Foundation
+
+final class SearchViewModel {
+    enum SortType {
+        case popular, newest
+    }
+    var sortType: SortType = .popular
+    
+    var models = [SearchModel]()
+    
+    private var isLoading: Bool = false {
+        didSet {
+            if isLoading && !oldValue { loadingAction?(true) }
+            if !isLoading && oldValue { loadingAction?(false) }
+        }
+    }
+    
+    var searchErrorAction: ((Error) -> ())?
+    var searchResultAction: ((SearchModel) -> ())?
+    var loadingAction: ((Bool) -> ())?
+    
+    private let networkManager = NetworkManager()
+    
+    func performSearchRequest(query: String) {
+        isLoading = true
+        networkManager.searchPhotos(query: query) { result in
+                self.isLoading = false
+                switch result {
+                case .success(let response):
+                    for result in response.results {
+                        self.performImageDataRequest(model: result)
+                    }
+                case .failure(let error):
+                    self.searchErrorAction?(error)
+                }
+        }
+    }
+    
+    private func performImageDataRequest(model: SearchResult) {
+        isLoading = true
+        networkManager.getImageData(url: model.urls.regular) { result in
+            self.isLoading = false
+                switch result {
+                case .success(let data):
+                    let model = SearchModel(id: model.id, imageData: data, description: model.description ?? "")
+                    self.searchResultAction?(model)
+                case .failure(let error):
+                    self.searchErrorAction?(error)
+                }
+        }
+    }
+}
