eef5532908803b116873302a04daeffc39e7c138
vtagilov <v.tagilov@mail.ru>
2025-02-10 13:38:45 +0300

    Initial Commit

---
+++ AvitoInternship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoInternship
+//
+//  Created by Владимир on 10.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
